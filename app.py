cc0ebcf4144a2fb2662a3f00c72d973fcdda4477
hoan3542 <khaichoan3542@gmail.com>
2025-07-16 23:10:40 +0700

    update app.py,index.html,results.html and add CSS

--- app.py
+++ app.py
@@ -9,23 +9,35 @@ def scrape_vnexpress(query):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     print(soup.prettify())
+
     results = []
     articles = soup.select("article.item-news")
-    for article in articles[:10]:
+
+    for article in articles[:5]:
         title_tag = article.select_one("h3.title-news a")
-        img_tag = article.select_one("img.thumb") or article.select_one("img.thumb-art") or article.select_one("img")
         desc_tag = article.select_one("p.description")
+
         if not title_tag:
             continue
 
         title = title_tag.get_text(strip=True)
         link = title_tag['href']
-        image = ''
-        if img_tag:
-            image = img_tag.get('data-src') or img_tag.get('src') or ''
-            if image.startswith('//'):
-                image = 'https:' + image
         summary = desc_tag.get_text(strip=True) if desc_tag else ''
+#xu ly anh        
+        picture_tag = article.select_one("div.thumb-art picture source")
+        image = ''
+        if picture_tag:
+            data_srcset = picture_tag.get('data-srcset')
+            if data_srcset:
+                image = data_srcset.split(' ')[0]
+        if not image:
+            picture_tag = article.select_one(".thumb-art img") or article.select_one("img")
+            if picture_tag:
+                image = picture_tag.get('data-src') or picture_tag.get('src') or ''
+        if image.startswith('//'):
+            image = 'https:' + image
+
+        print("Ảnh lấy được:", image)
 
         results.append({
             'title': title,
@@ -36,16 +48,43 @@ def scrape_vnexpress(query):
         })
 
     return results
+
 def scrape_tuoitre(query):
     url = f"https://tuoitre.vn/tim-kiem.htm?keywords={query.replace(' ', '+')}"
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
+    print(soup.prettify())
+
     results = []
     articles = soup.select("a.box-category-link-title")
+
     for article in articles[:5]:
-        title = article.get_text(strip=True)
+        title = article.get_text(strip=True) 
         link = "https://tuoitre.vn" + article['href']
-        results.append({'title': title, 'link': link, 'source': 'Tuổi Trẻ'})
+
+        if not title:
+            continue
+
+        desc_tag = article.find_next('p', class_='box-category-sapo')
+        summary = desc_tag.get_text(strip=True) if desc_tag else "Không có mô tả"
+#xu ly anh        
+        picture_tag = article.select_one(".box-category-link-with-avatar img.box-category-avatar")
+        image = ''
+        if picture_tag:
+            image = picture_tag.get('data-srcset') or picture_tag.get('srcset') or picture_tag.get('data-src') or picture_tag.get('src') or ''
+        if image.startswith('//'):
+            image = 'https:' + image
+
+        print("Ảnh lấy được:", image)
+
+        results.append({
+            'title': title,
+            'link': link,
+            'source': 'Tuổi Trẻ',
+            'image': image,
+            'summary': summary
+        })
+
     return results
 
 def scrape_thanhnien(query):
@@ -59,45 +98,48 @@ def scrape_thanhnien(query):
     for article in articles[:5]:
         title = article.get_text(strip=True)
         link = article['href']
-        if not link.startswith("http"):
-            link = "https://thanhnien.vn" + article['href']
-        results.append({'title': title, 'link': link, 'source': 'Thanh Niên'})
-    return results
-def scrape_tienphong(query):
-    url = f"https://tienphong.vn/tim-kiem/{query.replace(' ', '%20')}.html"
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    results = []
-    articles = soup.select("h2.story__heading a.cms-link")
 
-    for article in articles[:5]:
-        title = article.get_text(strip=True)
-        link = article['href']
-        if not link.startswith("http"):
-            link = "https://tienphong.vn/" + article['href']
-        results.append({'title': title, 'link': link, 'source': 'Tiền Phong'})
-    return results
+        if not title:
+            continue
 
-@app.route('/', methods=['GET', 'POST'])
+        desc_tag = article.find_next('a', attrs={"class": ["box-category-sapo", "d-block"]})
+        summary = desc_tag.get_text(strip=True) if desc_tag else "Không có mô tả"
+#xu ly anh
+        picture_tag = article.select_one("img.box-category-avatar")
+        image = ''
+        if picture_tag:
+            image = picture_tag.get('data-src') or picture_tag.get('src') or ''
+        if image.startswith('//'):
+            image = 'https:' + image
+
+        print("Ảnh lấy được:", image)
+        results.append({
+            'title': title,
+            'link': link,
+            'source': 'Thanh Niên',
+            'image': image,
+            'summary': summary
+        })
+        
+    return results
+@app.route('/', methods=['GET'])
 def index():
+    return render_template('index.html')
+
+@app.route('/results', methods=['GET'])
+def results():
     news_results = []
-    sources = []
-    if request.method == 'POST':
-        keyword = request.form['keyword']
-        location = request.form.get('location', '')
-        full_query = f"{keyword} {location}".strip()
-        sources = request.form.getlist('sources')
+    sources = request.args.getlist('sources')
+    keyword = request.args.get('keyword', '')
+    location = request.args.get('location', '')
+    full_query = f"{keyword} {location}".strip()
     if 'vnexpress' in sources:
         news_results += scrape_vnexpress(full_query)
     if 'tuoitre' in sources:
         news_results += scrape_tuoitre(full_query)
     if 'thanhnien' in sources:
         news_results += scrape_thanhnien(full_query)
-    if 'tienphong' in sources:
-        news_results += scrape_tienphong(full_query)
-
-    return render_template('index.html', results=news_results)
+    return render_template('results.html', results=news_results)
 
 if __name__ == '__main__':
     app.run(debug=True)
